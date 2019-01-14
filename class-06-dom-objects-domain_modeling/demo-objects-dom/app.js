fbff01b04d7e9afec183cb15b06c302c2d33eccf
Sam Hamm <sam@codefellows.com>
2019-01-14 11:21:44 -0800

    DOM demo starter

---
+++ class-06-dom-objects-domain_modeling/demo-objects-dom/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Object literal notation
+
+// Let's model some Seattle beaches in code! Take a look at the HTML to see which ones. Were going to take the seagull data from that we store in JavaScript and render it into the DOM.
+
+// Let's make note of the name, neighborhood, and seagull count of each beach for each day last week.
+
+// Since we know we'll need days of the week, that is already set up for us.
+
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
